7ee804039bd297133f5b01e86fa1216ebacea4e6
lughjw <lughjw@gmail.com>
2020-02-19 13:48:17 -0600

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index
+	end
+
+	get '/info' do
+		erb :info
+	end
+
+	get '/info/:id/apples' do
+		"I've got a lovely bunch of apples for you!"
 	end
 end
